ee5bc2325925f39f3b9718336116e3cbc092ad55
ViniciusSotana <vinysotana@gmail.com>
2024-06-20 14:03:45 -0300

    movimentação e pacientes mapa2

--- main.cpp
+++ main.cpp
@@ -177,8 +177,8 @@ int main(){
     bool redesenha=true;
     int posX = 32;
     int posY = 32;
-    int posX2 = 265;
-    int posY2 = 265;
+    int posX2 = 27 * BLOCKSIZE;
+    int posY2 = 3 * BLOCKSIZE;
 
     bool keys[8] = {false, false, false, false, false, false, false, false};
 
@@ -205,6 +205,7 @@ int main(){
     ALLEGRO_BITMAP *pacienteVermelho = al_load_bitmap("paciente_vermelho.png");
     ALLEGRO_BITMAP *pacienteLaranja = al_load_bitmap("paciente_laranja.png");
     ALLEGRO_BITMAP *pacienteRosa = al_load_bitmap("paciente_rosa.png");
+    ALLEGRO_BITMAP *pacienteCurado = al_load_bitmap("paciente_curado.png");
 
     //ALLEGRO_BITMAP *bloco2 = NULL;
     //ALLEGRO_BITMAP *bloco1 = NULL;
@@ -215,7 +216,6 @@ int main(){
     al_start_timer(timer);
 
     int cont = 0;
-    bool porta = false;
     bool p1 = true, p2 = true, p3 = true, p4 = true, p5 = true, p6 = true;
     bool concluiu = false;
 
@@ -223,9 +223,13 @@ int main(){
         ALLEGRO_EVENT events;
         al_wait_for_event(event_queue, &events);
         ALLEGRO_EVENT_TYPE tipoEvento=events.type;
+
+
+    if(mapaAtual == 1 || mapaAtual == 0){
         switch (tipoEvento) {
           case ALLEGRO_EVENT_TIMER: { redesenha=true; break;}
           case ALLEGRO_EVENT_DISPLAY_CLOSE: { done = true; break;}
+
           case ALLEGRO_EVENT_KEY_DOWN: {
             int tecla = events.keyboard.keycode;
             switch(tecla){
@@ -243,7 +247,6 @@ int main(){
                 case ALLEGRO_KEY_DOWN: {keys[DOWN] = false;break; }
                 case ALLEGRO_KEY_LEFT: {keys[LEFT] = false; break;}
                 case ALLEGRO_KEY_RIGHT: {keys[RIGHT] = false; break;}
-                case ALLEGRO_KEY_ESCAPE: {done = true; break; }
             }
             break;
           }
@@ -260,55 +263,78 @@ int main(){
         if (!colisao(posX + 2, posY)){
            posX += keys[RIGHT] * 2;
         }
+    }else{
 
-    if(events.type == ALLEGRO_EVENT_KEY_DOWN){
-            switch(events.keyboard.keycode){
-                case ALLEGRO_KEY_W:
-                    keys[W] = true;
-                    break;
-                case ALLEGRO_KEY_S:
-                    keys[S] = true;
-                    break;
-                case ALLEGRO_KEY_A:
-                    keys[A] = true;
-                    break;
-                case ALLEGRO_KEY_D:
-                    keys[D] = true;
-                    break;
+        switch (tipoEvento) {
+    case ALLEGRO_EVENT_TIMER: {
+        redesenha = true;
+        break;
+    }
+    case ALLEGRO_EVENT_DISPLAY_CLOSE: {
+        done = true;
+        break;
+    }
+    case ALLEGRO_EVENT_KEY_DOWN: {
+        int tecla = events.keyboard.keycode;
+        switch (tecla) {
+            case ALLEGRO_KEY_W: {
+                keys[W] = true;
+                break;
+            }
+            case ALLEGRO_KEY_S: {
+                keys[S] = true;
+                break;
+            }
+            case ALLEGRO_KEY_A: {
+                keys[A] = true;
+                break;
+            }
+            case ALLEGRO_KEY_D: {
+                keys[D] = true;
+                break;
             }
         }
-        else if(events.type == ALLEGRO_EVENT_KEY_UP) {
-            switch(events.keyboard.keycode){
-                case ALLEGRO_KEY_W:
-                    keys[W] = false;
-                    break;
-                case ALLEGRO_KEY_S:
-                    keys[S] = false;
-                    break;
-                case ALLEGRO_KEY_A:
-                    keys[A] = false;
-                    break;
-                case ALLEGRO_KEY_D:
-                    keys[D] = false;
-                    break;
-                case ALLEGRO_KEY_ESCAPE:
-                    done = true;
-                    break;
+        break;
+    }
+    case ALLEGRO_EVENT_KEY_UP: {
+        int tecla = events.keyboard.keycode;
+        switch (tecla) {
+            case ALLEGRO_KEY_W: {
+                keys[W] = false;
+                break;
+            }
+            case ALLEGRO_KEY_S: {
+                keys[S] = false;
+                break;
+            }
+            case ALLEGRO_KEY_A: {
+                keys[A] = false;
+                break;
+            }
+            case ALLEGRO_KEY_D: {
+                keys[D] = false;
+                break;
             }
         }
+        break;
+    }
+}
 
-        if (!colisao(posX2, posY2 - 2)){
-           posY2 -= keys[W] * 2;
-        }
-        if (!colisao(posX2, posY2 + 2)){
-           posY2 += keys[S] * 2;
-        }
-        if (!colisao(posX2 - 2, posY2)){
-           posX2 -= keys[A] * 2;
-        }
-        if (!colisao(posX2 + 2, posY2)){
-           posX2 += keys[D] * 2;
-        }
+
+if (!colisao(posX2, posY2 - 2)) {
+    posY2 -= keys[W] * 2;
+}
+if (!colisao(posX2, posY2 + 2)) {
+    posY2 += keys[S] * 2;
+}
+if (!colisao(posX2 - 2, posY2)) {
+    posX2 -= keys[A] * 2;
+}
+if (!colisao(posX2 + 2, posY2)) {
+    posX2 += keys[D] * 2;
+}
+    }
+
 
         if (redesenha && cont < 1) {
             DrawMap0();
@@ -368,6 +394,7 @@ int main(){
             al_flip_display();
             redesenha = false;
         }   else if (redesenha && cont == 1) {
+
             if(concluiu == false){
             mapaAtual = 1;
           DrawMap1();
@@ -384,78 +411,75 @@ int main(){
           posX = BLOCKSIZE * 24;
           posY = BLOCKSIZE * 3;
 
-          al_draw_bitmap(player, posX, posY, 0);
-
           colisao(posX, posY);
 
           al_flip_display();
           redesenha=false;
           concluiu = true;
+          p1 = true, p2 = true, p3 = true, p4 = true, p5 = true, p6 = true;
                     }
                 }else{
-                mapaAtual = 2;
-          DrawMap2();
-          posX2 = BLOCKSIZE * 24;
-          posY2 = BLOCKSIZE * 3;
-          p1 = true, p2 = true, p3 = true, p4 = true, p5 = true, p6 = true;
+                        DrawMap2();
 
-          al_draw_bitmap(player2, posX2, posY2, 0);
+                      colisao(posX2, posY2);
 
-          if (posX2 >= (4 * BLOCKSIZE) && posX2 <= (5 * BLOCKSIZE) && posY2 >= (21 * BLOCKSIZE) && posY2 <= (22 * BLOCKSIZE) && p1) {
-                p1 = false;
-                cont++;
-            }
-            if (p1) {
-                al_draw_bitmap(pacienteAmarelo, (5 * BLOCKSIZE), (21 * BLOCKSIZE), 0);
-            }
+                      al_draw_bitmap(player2, posX2, posY2, 0);
 
-            if (posX2 >= (6 * BLOCKSIZE) && posX2 <= (7 * BLOCKSIZE) && posY2 >= (14 * BLOCKSIZE) && posY2 <= (15 * BLOCKSIZE) && p2) {
-                p2 = false;
-                cont++;
-            }
-            if (p2) {
-                al_draw_bitmap(pacienteRoxo, (5 * BLOCKSIZE), (14 * BLOCKSIZE), 0);
-            }
+                      if (posX2 >= (4 * BLOCKSIZE) && posX2 <= (5 * BLOCKSIZE) && posY2 >= (21 * BLOCKSIZE) && posY2 <= (22 * BLOCKSIZE) && p1) {
+                            p1 = false;
+                        }
+                        if (p1) {
+                            al_draw_bitmap(pacienteAmarelo, (5 * BLOCKSIZE), (21 * BLOCKSIZE), 0);
+                        }else{
+                            al_draw_bitmap(pacienteCurado, (5 * BLOCKSIZE), (21 * BLOCKSIZE), 0);}
 
-            if (posX2 >= (21 * BLOCKSIZE) && posX2 <= (22 * BLOCKSIZE) && posY2 >= (22 * BLOCKSIZE) && posY2 <= (23 * BLOCKSIZE) && p3) {
-                p3 = false;
-                cont++;
-            }
-            if (p3) {
-                al_draw_bitmap(pacienteVerde, (21 * BLOCKSIZE), (22 * BLOCKSIZE), 0);
-            }
+                        if (posX2 >= (6 * BLOCKSIZE) && posX2 <= (7 * BLOCKSIZE) && posY2 >= (14 * BLOCKSIZE) && posY2 <= (15 * BLOCKSIZE) && p2) {
+                            p2 = false;
+                        }
+                        if (p2) {
+                            al_draw_bitmap(pacienteRoxo, (5 * BLOCKSIZE), (14 * BLOCKSIZE), 0);
+                        }else{
+                            al_draw_bitmap(pacienteCurado, (5 * BLOCKSIZE), (14 * BLOCKSIZE), 0);}
 
-            if (posX2 >= (31 * BLOCKSIZE) && posX2 <= (32 * BLOCKSIZE) && posY2 >= (22 * BLOCKSIZE) && posY2 <= (23 * BLOCKSIZE) && p4) {
-                p4 = false;
-                cont++;
-            }
-            if (p4) {
-                al_draw_bitmap(pacienteVermelho, (31 * BLOCKSIZE), (22 * BLOCKSIZE), 0);
-            }
+                        if (posX2 >= (21 * BLOCKSIZE) && posX2 <= (22 * BLOCKSIZE) && posY2 >= (22 * BLOCKSIZE) && posY2 <= (23 * BLOCKSIZE) && p3) {
+                            p3 = false;
+                        }
+                        if (p3) {
+                            al_draw_bitmap(pacienteVerde, (21 * BLOCKSIZE), (22 * BLOCKSIZE), 0);
+                        }else{
+                            al_draw_bitmap(pacienteCurado, (21 * BLOCKSIZE), (22 * BLOCKSIZE), 0);}
 
-            if (posX2 >= (45 * BLOCKSIZE) && posX2 <= (46 * BLOCKSIZE) && posY2 >= (14 * BLOCKSIZE) && posY2 <= (15 * BLOCKSIZE) && p5) {
-                p5 = false;
-                cont++;
-            }
-            if (p5) {
-                al_draw_bitmap(pacienteLaranja, (45 * BLOCKSIZE), (14 * BLOCKSIZE), 0);
-            }
+                        if (posX2 >= (31 * BLOCKSIZE) && posX2 <= (32 * BLOCKSIZE) && posY2 >= (22 * BLOCKSIZE) && posY2 <= (23 * BLOCKSIZE) && p4) {
+                            p4 = false;
+                        }
+                        if (p4) {
+                            al_draw_bitmap(pacienteVermelho, (31 * BLOCKSIZE), (22 * BLOCKSIZE), 0);
+                        }else{
+                            al_draw_bitmap(pacienteCurado, (31 * BLOCKSIZE), (22 * BLOCKSIZE), 0);}
 
-            if (posX2 >= (45 * BLOCKSIZE) && posX2 <= (46 * BLOCKSIZE) && posY2 >= (21 * BLOCKSIZE) && posY <= (22 * BLOCKSIZE) && p6) {
-                p6 = false;
-                cont++;
-            }
-            if (p6) {
-                al_draw_bitmap(pacienteRosa, (45 * BLOCKSIZE), (21 * BLOCKSIZE), 0);
-            }
+                        if (posX2 >= (45 * BLOCKSIZE) && posX2 <= (46 * BLOCKSIZE) && posY2 >= (14 * BLOCKSIZE) && posY2 <= (15 * BLOCKSIZE) && p5) {
+                            p5 = false;
+                        }
+                        if (p5) {
+                            al_draw_bitmap(pacienteLaranja, (45 * BLOCKSIZE), (14 * BLOCKSIZE), 0);
+                        }else{
+                            al_draw_bitmap(pacienteCurado, (45 * BLOCKSIZE), (14 * BLOCKSIZE), 0);}
 
-          colisao(posX2, posY2);
+                        if (posX2 >= (45 * BLOCKSIZE) && posX2 <= (46 * BLOCKSIZE) && posY2 >= (21 * BLOCKSIZE) && posY <= (22 * BLOCKSIZE) && p6) {
+                            p6 = false;
+                        }
+                        if (p6) {
+                            al_draw_bitmap(pacienteRosa, (45 * BLOCKSIZE), (21 * BLOCKSIZE), 0);
+                        }else{
+                            al_draw_bitmap(pacienteCurado, (45 * BLOCKSIZE), (21 * BLOCKSIZE), 0);}
 
-          al_flip_display();
-          redesenha=false;
-            }
+
+                      al_flip_display();
+                      redesenha=false;
             }
         }
+    }
+
 
     al_destroy_display(display);
     al_destroy_timer(timer);
